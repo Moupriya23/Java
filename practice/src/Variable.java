db7eb5fe909353ec5b5450e34b99f2f5574fab2b
Moupriya Roy <moupriyaroy2001@gmail.com>
2023-12-26 14:36:38 +0530

    change available
    change available

---
+++ practice/src/Variable.java
@@ -0,0 +1,16 @@
+public class Variable {
+    public static void main(String[] args) {
+
+        char a = 'A';
+        String name = "Moupriya";
+        int num = 2023;
+        double number = 20.23;
+        float b = 25;
+
+                System.out.println(name);
+                System.out.println(a);
+                System.out.println(num);
+                System.out.println(number);
+                System.out.println(b);
+    }
+}
